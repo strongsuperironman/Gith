34906307c03227e6320b782ea3c351b83f2dd22d
liuzhen <1737396870@qq.com>
2018-06-28 19:51:28 +0800

    tijiaohelp

---
+++ tijiao.py
@@ -0,0 +1,13 @@
+1.#进入他人的github仓库进行fork,这样就把仓库给传递到自己的远端;
+2.git remote add "他人名字""Https://github.com/'用户名'/仓库名.git"
+3.git remote add "自己名字""Https://github.com/'自己用户名'/同上仓库名.git"
+4.git remote -v#查看连接是否成功
+5.git pull "自己名字"master
+6.vim +"文件名"#可以是新增文件也可以是仓库文件
+7.git add "文件名"
+8.git commit -m "修改注释"
+9.git push "自己名字" master
+10 "进入github官网 pullrequests---New pullrequest----creat pull request"
+11."等待被上传的人接受"
+12."查看他人自己上传文件的修改意见"git pull"他人姓名"master
+
